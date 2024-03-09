d32d49855a22acab3049808e1b94028dd229512a
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2024-03-09 14:29:17 +0500

    homewrok

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Home() {
+
+  
+
+  useEffect(() => {
+    fetch("E1MC8RZ-SB4MAVP-M1XD5EN-5DGPZ1P")
+      .then(res => res.json())
+      .then((el) => {
+        console.log(el);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      
+
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>
+      Trending
+    </div>
   )
 }
 

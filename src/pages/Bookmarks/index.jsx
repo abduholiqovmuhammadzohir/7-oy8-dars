7004cd12bc6d92976b7bc2918e476899bb5686e0
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2024-03-11 16:27:21 +0500

    homewrok

--- src/pages/Bookmarks/index.jsx
+++ src/pages/Bookmarks/index.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import Card from "../../components/card";
+export default function Home() {
+  const [data, setData] = useState([]);
+  let [reck, setreck] = useState([]);
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    setLoader(true);
+    const apiKey = "E1MC8RZ-SB4MAVP-M1XD5EN-5DGPZ1P";
+    fetch("https://api.kinopoisk.dev/v1.4/movie?lists=top250", {
+      headers: {
+        "X-API-KEY": apiKey,
+      },
+    })
+      .then((response) => response.json())
+      .then((el) => {
+        setData(el.docs);
+        setLoader(false);
+      })
+      .catch((error) => console.error("Error:", error));
 
-function Bookmarks() {
+    fetch("https://api.kinopoisk.dev/v1.4/list?page=4&limit=20", {
+      headers: {
+        "X-API-KEY": apiKey,
+      },
+    })
+      .then((response) => response.json())
+      .then((el) => setreck(el.docs))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  console.log(reck);
   return (
-    <div>Bookmarks</div>
-  )
+    <>
+      <div>
+        <h2>Trending</h2>
+        {loader ? (
+          <p>loding</p>
+        ) : (
+          <div className="carousel carousel-center w-full p-2  to-transparent rounded-box gap-6 ">
+            {reck.map((el, index) => {
+              return <CaruselCard key={index} el={el} />;
+            })}
+          </div>
+         )} 
+        <div className="content-main">
+          <section className="text-gray-600 body-font">
+            <div className="container px-5 py-14 mx-auto">
+              <div className="flex flex-wrap gap-9">
+                {reck.map((el, index) => {
+                  return <Card key={index} el={el} />;
+                })}
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default Bookmarks
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,61 @@
-import React, { useEffect, useState } from 'react'
-
-function Home() {
-
-  
-
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import Card from "../../components/card";
+export default function Home() {
+  const [data, setData] = useState([]);
+  let [reck, setreck] = useState([]);
+  const [loader, setLoader] = useState(true);
   useEffect(() => {
-    fetch("E1MC8RZ-SB4MAVP-M1XD5EN-5DGPZ1P")
-      .then(res => res.json())
+    setLoader(true);
+    const apiKey = "E1MC8RZ-SB4MAVP-M1XD5EN-5DGPZ1P";
+    fetch("https://api.kinopoisk.dev/v1.4/movie?lists=top250", {
+      headers: {
+        "X-API-KEY": apiKey,
+      },
+    })
+      .then((response) => response.json())
       .then((el) => {
-        console.log(el);
+        setData(el.docs);
+        setLoader(false);
       })
-      .catch((err) => {
-        console.log(err);
-      })
-      
+      .catch((error) => console.error("Error:", error));
 
-  }, [])
+    fetch("https://api.kinopoisk.dev/v1.4/list?page=4&limit=20", {
+      headers: {
+        "X-API-KEY": apiKey,
+      },
+    })
+      .then((response) => response.json())
+      .then((el) => setreck(el.docs))
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
+  console.log(reck);
   return (
-    <div>
-      Trending
-    </div>
-  )
+    <>
+      <div>
+        <h2>Trending</h2>
+        {loader ? (
+          <p>loding</p>
+        ) : (
+          <div className="carousel carousel-center w-full p-2  to-transparent rounded-box gap-6 ">
+            {reck.map((el, index) => {
+              return <CaruselCard key={index} el={el} />;
+            })}
+          </div>
+         )} 
+        <div className="content-main">
+          <section className="text-gray-600 body-font">
+            <div className="container px-5 py-14 mx-auto">
+              <div className="flex flex-wrap gap-9">
+                {reck.map((el, index) => {
+                  return <Card key={index} el={el} />;
+                })}
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default Home
